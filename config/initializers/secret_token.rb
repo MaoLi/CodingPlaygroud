6bccb7884c71c08d83df01f6a3a37aa22838d5a1
Mao Li <stcatzlee@gmail.com>
2011-12-11 14:35:58 +0800

    a new blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weblog::Application.config.secret_token = 'd3d35c70fea0572455106e88915e1b2c78558d68fbee442ae8675d1d70a12782ee7f8da28d9d2bc32b9827b8158cb1cfd61f2876221906d19f22bacbbd442c09'
